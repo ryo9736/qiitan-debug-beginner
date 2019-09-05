d26bcb8873ff2881f83f1e7dd7e5f837828d8d79
yas-github <36231225+yas-github@users.noreply.github.com>
2019-09-05 20:17:10 +0900

    Update account_activations_controller.rb

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,7 +3,8 @@ class AccountActivationsController < ApplicationController
 
   def edit
     user = User.find_by(email: params[:email])
-    if user
+    if user && !user.activated? && user.authenticated?(:activate, params[:id])
+      user.activate
       log_in user
       redirect_to root_path, notice: 'アカウントが有効になりました'
     else
